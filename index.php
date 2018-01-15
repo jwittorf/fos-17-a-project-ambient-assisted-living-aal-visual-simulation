548f657a1841ddfb1e47655764bc145c0f60d979
Julian Wittorf <jwittorf@die-netzwerkstatt.de>
2018-01-15 23:15:03 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+	<title>A1 (AAL) - Simulation - RBZ Technik</title>
+	<!-- Bootstrap -->
+	<link href="assets/Public/Stylesheets/main.css" rel="stylesheet">
+</head>
+<body>
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="assets/Public/Javascripts/vendor/jquery.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="assets/Public/Javascripts/vendor/bootstrap.js"></script>
+<script src="assets/Public/Javascripts/main.js"></script>
+</body>
+</html>
