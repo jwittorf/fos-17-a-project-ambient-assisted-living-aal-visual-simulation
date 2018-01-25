1341f00d5587330e435c091489237ab9aa69fcb6
Julian Wittorf <jwittorf@die-netzwerkstatt.de>
2018-01-25 13:17:25 +0100

    [BUGFIX] Allow check for globalControlExclude and windowControlClass in timer

--- web/assets/Javascripts/jquery.main.js
+++ web/assets/Javascripts/jquery.main.js
@@ -401,7 +401,7 @@
 					// We want to enable and disable everything, that doesn't need constant electricity
 					// Add special case of class, to less time to develop proper solution
 					// FOS TODO: By time, check for the classes in the array, not just the ids
-					if (($.inArray(toggleLocalControl, globalControlResetExclude) === -1) && ($.inArray("." + windowControlClass, globalControlResetExclude) === -1)) {
+					if (($.inArray(toggleLocalControl, globalControlResetExclude) === -1) || ($.inArray("." + windowControlClass, globalControlResetExclude) === -1)) {
 						// Add current toggle local control to array of globalTimerToggles as jQuery object
 						globalTimerToggles.push($toggleLocalControl);
 					}
